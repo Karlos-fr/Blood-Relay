1a21cc931d9b86059339b9f6fd5768fcaee2975c
Karlos-fr <rezuss@hotmail.com>
2026-08-30 07:51:15 +0200

    layout: widen game viewport

--- src/config/game.ts
+++ src/config/game.ts
@@ -1,6 +1,6 @@
 import { ARENA_CONTENT_SCALE } from './arenaScale';
 
-export const GAME_WIDTH = 960;
+export const GAME_WIDTH = 1120;
 export const GAME_HEIGHT = 540;
 export const GAME_VERSION = '0.1.0-dev';
 export const GRAVITY_Y = 900 * ARENA_CONTENT_SCALE;
